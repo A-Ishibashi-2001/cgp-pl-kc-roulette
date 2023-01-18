d4dbebe244cfe5040ec8b13d5e6b1f4ffaf4df33
Akihiro ISHIBASHI <89733374+Ishibashi-cgp@users.noreply.github.com>
2023-01-19 02:37:32 +0900

    make roulette system from https://qiita.com/Jackpackeeer/items/d3eb65fc9c4ba992db24

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import {Roulette} from './Roulette'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Roulette></Roulette>
     </div>
   );
 }
---
+++ src/Roulette.js
@@ -0,0 +1,49 @@
+import {  useState, useEffect, useCallback, memo } from "react";
+
+export const Roulette = memo(() => {
+    const [start, setStart] = useState(false);
+    const [index, setIndex] = useState(0);
+
+    const rouletteContents = [
+        "カレー",
+        "パスタ",
+        "唐揚げ",
+        "天ぷら",
+        "中華",
+        "ハンバーグ",
+        "うどん",
+        "肉じゃが"
+    ];
+
+    //ボタンの文言を変更する処理
+    const startRoulette = useCallback(() => {
+        setStart(!start);
+    }, [start]);
+
+    //ルーレットを回す処理
+    useEffect(() => {
+        if (start) {
+            const interval = setInterval(() => {
+                setIndex((oldIndex) => {
+                    if (oldIndex < rouletteContents.length - 1) return oldIndex + 1;
+                    return 0;
+                });
+            }, 50);//ルーレットの中身を切り替える速度
+            return () => clearInterval(interval);
+        } else if (!start) {
+            return () => clearInterval();
+        }
+    }, [start]);
+
+    return (
+        <>
+            <div>
+                <p>今日のメニューは・・・</p>
+                <p>{rouletteContents[index]}</p>
+            </div>
+            <button type="button" onClick={startRoulette}>
+                {start ? "ストップ" : "スタート"}
+            </button>
+        </>
+    );
+});
